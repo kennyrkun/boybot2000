2640f3c3de76d11dff4e85afcf7a0350aac0814a
Kenny McCormick <kennyrkun@users.noreply.github.com>
2026-05-16 21:34:58 -0500

    reply mad as hell to clanker

--- cogs/Boytoy.py
+++ cogs/Boytoy.py
@@ -67,6 +67,9 @@ class Boytoy(commands.Cog):
                 return await message.add_reaction("<:boykisser_mad_as_hell:1488617115694006352>")
             else:
                 return await message.add_reaction("<:boykisser_what:1483293684899381248>")
+
+        elif any(x in messageText for x in [ "clanker" ]):
+            return await message.add_reaction("<:boykisser_mad_as_hell:1488617115694006352>")
         
         # TODO: had to remove "boy" from this because it would reply to boykisser emotes
         elif any(x in messageText for x in [ "boys" ]):
