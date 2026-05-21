a5604f5872aaae7f536167e920446d9107ccd6e3
Kenny McCormick <kennyrkun@users.noreply.github.com>
2026-05-21 18:23:10 -0500

    temporarily disable indirect thing

--- cogs/Boytoy.py
+++ cogs/Boytoy.py
@@ -76,10 +76,11 @@ class Boytoy(commands.Cog):
 
                     promptData = {
                         "prompt":
-                            """
-                                You will be given a message to read. If the message is directed AT you, reply to it normally.
-                                If the message is talking ABOUT you, but not directly to you, reply with only and exactly with "Indirect" and nothing else. Otherwise, reply normally.
-                            """,
+                            #"""
+                            #    You will be given a message to read. If the message is directed AT you, reply to it normally.
+                            #    If the message is talking ABOUT you, but not directly to you, reply with only and exactly with "Indirect" and nothing else. Otherwise, reply normally.
+                            #""",
+                            "Reply to the following message.",
                         "content": message.content,
                         "images": []
                     }
