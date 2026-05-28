78c1b4a204a8b8f2c2782fb59c55041d0120fac5
Kenny McCormick <kennyrkun@users.noreply.github.com>
2026-05-28 16:19:52 -0500

    don't drop captcha queue

--- store.py
+++ store.py
@@ -26,7 +26,6 @@ class Store:
         )
         cur.execute("CREATE UNIQUE INDEX IF NOT EXISTS name_and_guildid ON extensions_enabled (name, guild_id)")
 
-        cur.execute("DROP TABLE captcha_queue")
         cur.execute(
             """
             CREATE TABLE IF NOT EXISTS captcha_queue (
