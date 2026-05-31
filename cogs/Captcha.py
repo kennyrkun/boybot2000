12fb620e93a0c5aeb1c0ca9610be876a938082e8
Kenny McCormick <kennyrkun@users.noreply.github.com>
2026-05-31 11:26:04 -0500

    strptime expects a string

--- cogs/Captcha.py
+++ cogs/Captcha.py
@@ -128,7 +128,7 @@ class Captcha(commands.Cog):
             for user in queuedUsers:
                 # TODO: if the user timestamp in the db is earlier than the user's server join date, remove them from the queue
 
-                date = datetime.strptime(int(user["timestamp"]), "%Y-%m-%d %H:%M:%S.%f")
+                date = datetime.strptime(user["timestamp"], "%Y-%m-%d %H:%M:%S.%f")
 
                 if date > timeoutTimestamp:
                     # get guild from id and kick the user
