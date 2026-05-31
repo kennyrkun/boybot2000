7b0fa452871107028f7cc84e2420809687bbfeb3
Kenny McCormick <kennyrkun@users.noreply.github.com>
2026-05-31 11:01:59 -0500

    fix captcha can't do fix thing

--- cogs/Captcha.py
+++ cogs/Captcha.py
@@ -128,7 +128,7 @@ class Captcha(commands.Cog):
             for user in queuedUsers:
                 # TODO: if the user timestamp in the db is earlier than the user's server join date, remove them from the queue
 
-                if datetime.fromtimestamp(user["timestamp"]) > timeoutTimestamp:
+                if datetime.fromtimestamp(int(user["timestamp"])) > timeoutTimestamp:
                     # get guild from id and kick the user
                     guild = self.bot.fetch_guild(user["guild_id"])
 
